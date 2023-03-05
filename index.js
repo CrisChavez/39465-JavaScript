2cc623a1032b099712c351c87b90c5735375e81a
Cristian Ariel Chavez <cristianarielchavez@outlook.com.ar>
2023-03-04 21:44:33 -0300

    se corrijen los else if

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-window.onload = iniciar;
+ window.onload = iniciar;
 
-function iniciar() {
-    let btnCalcular = document.getElementById("btnCalcular");
-    btnCalcular.addEventListener ("click", clickBtnCalcular);
-}
+ function iniciar() {
+     let btnCalcular = document.getElementById("btnCalcular");
+     btnCalcular.addEventListener ("click", clickBtnCalcular);
+ }
 
-function clickBtnCalcular() {
-    let txtPeso = document.getElementById ("txtPeso");
-    let peso = txtPeso.value;
+ function clickBtnCalcular() {
+     let txtPeso = document.getElementById ("txtPeso");
+     let peso = txtPeso.value;
 
     let txtAltura = document.getElementById ("txtAltura");
     let altura = txtAltura.value;
 
-    let imc = peso / (altura * altura);
+    // const peso = prompt ("Ingrese su Peso");
+    // const altura = prompt ("Ingrese su Altura");
+
+    const imc = peso / (altura * altura);
     alert("Su IMC es: " + Math.round(imc));
 
-    if (imc < 18.5){
+    if (imc < 18.5) {
         alert("Usted Tiene Poco Peso");
 
-    }     else if (imc >18.5 <25){
+    }     else if (imc >= 18.5 && imc < 25) {
         alert("Usted Se Encuentra en Parámetros Saludables");
 
-    }     else if (imc >25 <30){
+    }     else if (imc >= 25 && imc < 30) {
         alert("Usted Tiene Sobrepeso");
 
-    }     else if (imc >30 <35){
+    }     else if (imc >= 30 && imc < 35) {
         alert("Usted Tiene Obesidad I");
 
-    }     else if (imc >35 <40){
+    }     else if (imc >= 35 && imc < 40) {
         alert("Usted Tiene Obesidad II");
 
-    }     else if (imc >= 40){
+    }     else if (imc >= 40) {
         alert("Usted Tiene Obesidad III");
+
+    } else {
+        alert("Error en Valores Ingresados (Intente Ingresar la Altura en este formato: 1.70");
     } 
-}
+
+ }
